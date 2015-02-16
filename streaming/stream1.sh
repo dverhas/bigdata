269016f1067433666db02be9c4bf629efb328778
Vagrant <vagrant@nowhere.com>
2015-02-16 02:07:30 +0000

    add path for the streaming hadoop class so it can be run from the home directory

--- streaming/stream1.sh
+++ streaming/stream1.sh
@@ -1,4 +1,7 @@
-hadoop/bin/hadoop  jar ./hadoop-streaming-2.6.0.jar \
+rm -rf input/stream1
+rm -rf output/stream1
+cp bigdata/data/alice.txt input/stream1
+hadoop/bin/hadoop  jar ./bigdata/hadoop/contrib/hadoop-streaming-2.6.0.jar \
     -input input/stream1 \
     -output output/stream1 \
     -mapper /bin/cat \
--- streaming/stream2.sh
+++ streaming/stream2.sh
@@ -1,9 +1,10 @@
 rm -rf input/stream2
 rm -rf output/stream2
 cp bigdata/data/moby.txt input/stream2
-hadoop/bin/hadoop  jar ./hadoop-streaming-2.6.0.jar \
+#hadoop/bin/hadoop  jar ./hadoop-streaming-2.6.0.jar \
+hadoop/bin/hadoop  jar ./bigdata/hadoop/contrib/hadoop-streaming-2.6.0.jar \
     -input input/stream2 \
     -output output/stream2 \
-    -mapper ./mapper.py \
-    -reducer ./reducer.py
+    -mapper ./bigdata/streaming/mapper.py \
+    -reducer ./bigdata/streaming/reducer.py
 
